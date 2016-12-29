2b813ee512f56fc97708eb66babbb7eb56a588e9
coverdark <coverdark@live.cn>
2016-12-29 16:37:30 +0800

    add l1 loss

--- laa_z_y.py
+++ laa_z_y.py
@@ -6,8 +6,8 @@ import sys
 # read data
 # filename = "web_processed_data_feature_2"
 # filename = "age_data_3_category"
-# filename = "bluebird_data"
-filename = "flower_data"
+filename = "bluebird_data"
+# filename = "flower_data"
 data_all = np.load(filename+'.npz')
 user_labels = data_all['user_labels']
 label_mask = data_all['label_mask']
@@ -45,6 +45,10 @@ with tf.variable_scope('encoder_x_z'):
         tf.add(tf.matmul(x, weights), biases))
     print "x -> z, OK"
     
+    # loss
+    loss_z_entropy = - tf.reduce_mean(tf.reduce_sum(tf.mul(z, tf.log(z+1e-10)), reduction_indices=1))
+    loss_z_mean = tf.reduce_sum(tf.square((tf.reduce_mean(z, reduction_indices=0) - 1.0/n_z*np.ones(shape=[1, n_z]))))
+    
 # x, z -> y    
 with tf.variable_scope('encoder_x_z_y'):
     weights = [tf.Variable(
@@ -67,11 +71,15 @@ with tf.variable_scope('encoder_x_z_y'):
     constraint_template_classifier = np.matlib.repmat(np.eye(category_size), source_num, 1)
     for i in range(n_z):
         if i == 0:
-            loss_w_classifier = tf.nn.l2_loss(weights[i] - constraint_template_classifier)
-            loss_b_classifier = tf.nn.l2_loss(biases[i] - np.zeros([category_size]))
+            loss_w_classifier_l2 = tf.nn.l2_loss(weights[i] - constraint_template_classifier)
+            loss_w_classifier_l1 = tf.reduce_sum(tf.abs(weights[i]))
+            loss_b_classifier_l2 = tf.nn.l2_loss(biases[i] - np.zeros([category_size]))
+            loss_b_classifier_l1 = tf.reduce_sum(tf.abs(biases[i]))
         else:
-            loss_w_classifier += tf.nn.l2_loss(weights[i] - constraint_template_classifier)
-            loss_b_classifier += tf.nn.l2_loss(biases[i] - np.zeros([category_size]))
+            loss_w_classifier_l2 += tf.nn.l2_loss(weights[i] - constraint_template_classifier)
+            loss_w_classifier_l1 += tf.reduce_sum(tf.abs(weights[i]))
+            loss_b_classifier_l2 += tf.nn.l2_loss(biases[i] - np.zeros([category_size]))
+            loss_b_classifier_l1 += tf.reduce_sum(tf.abs(biases[i]))
 
 # y, z -> x
 with tf.variable_scope('decoder_yz_x'):
@@ -106,11 +114,15 @@ with tf.variable_scope('decoder_yz_x'):
     constraint_template_decoder = np.matlib.repmat(np.eye(category_size), 1, source_num)
     for i in range(n_z):
         if i == 0:
-            loss_w_decoder = tf.nn.l2_loss(weights[i] - constraint_template_decoder)
-            loss_b_decoder = tf.nn.l2_loss(biases[i] - np.zeros([input_size]))
+            loss_w_decoder_l2 = tf.nn.l2_loss(weights[i] - constraint_template_decoder)
+            loss_w_decoder_l1 = tf.reduce_sum(tf.abs(weights[i]))
+            loss_b_decoder_l2 = tf.nn.l2_loss(biases[i] - np.zeros([input_size]))
+            loss_b_decoder_l1 = tf.reduce_sum(tf.abs(biases[i]))
         else:
-            loss_w_decoder += tf.nn.l2_loss(weights[i] - constraint_template_decoder)
-            loss_b_decoder += tf.nn.l2_loss(biases[i] - np.zeros([input_size]))
+            loss_w_decoder_l2 += tf.nn.l2_loss(weights[i] - constraint_template_decoder)
+            loss_w_decoder_l1 += tf.reduce_sum(tf.abs(weights[i]))
+            loss_b_decoder_l2 += tf.nn.l2_loss(biases[i] - np.zeros([input_size]))
+            loss_b_decoder_l1 += tf.reduce_sum(tf.abs(biases[i]))
 
 # loss VAE by given y_prob
 y_prob = tf.placeholder(dtype=tf.float32, shape=(batch_size, category_size))
@@ -129,10 +141,14 @@ loss_classifier_y_x = tf.reduce_mean(tf.reduce_sum(_tmp_loss_backprop, reduction
 y_prior = tf.placeholder(dtype=tf.float32, shape=(batch_size, category_size))
 loss_y_kl = tf.reduce_mean(tf.reduce_sum(tf.mul(y, tf.log(1e-10 + y)) - tf.mul(y, tf.log(1e-10 + y_prior)), reduction_indices=1))
 y_kl_strength = tf.placeholder(dtype=tf.float32)
-loss_classifier = loss_classifier_y_x + y_kl_strength * loss_y_kl + 0.001/source_num/category_size/category_size * (loss_w_classifier + loss_w_decoder)
+loss_classifier = loss_classifier_y_x \
+    + y_kl_strength*loss_y_kl \
+    + 0.005/source_num/category_size/category_size * (loss_w_classifier_l2 + loss_b_classifier_l2 + loss_w_decoder_l2 + loss_b_decoder_l2) \
+    + 0.005/source_num/category_size/category_size * (loss_w_classifier_l1 + loss_b_classifier_l1 + loss_w_decoder_l1 + loss_b_decoder_l1) \
+    + 0.05*loss_z_entropy
 
 # optimizer
-learning_rate = 0.0005
+learning_rate = 0.005
 optimizer_classifier_x_y = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss_classifier_x_y)
 # optimizer_VAE = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss_VAE)
 optimizer_classifier = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss_classifier)
@@ -146,7 +162,7 @@ hit_num = tf.reduce_sum(tf.to_int32(tf.equal(inferred_category, y_label)))
 with tf.Session() as sess:
     tf.initialize_all_variables().run()
     # initialize x -> y
-    epochs = 100
+    epochs = 10
     total_batches = int(n_samples / batch_size)
     for epoch in xrange(epochs):
         total_cost = 0.0
@@ -182,13 +198,17 @@ with tf.Session() as sess:
                 feed_dict={x:batch_x, mask:batch_mask, y_label:batch_y_label, y_prior:batch_majority_y, y_kl_strength:0.0001})
              
             total_hit += batch_hit_num
- 
-#             if epoch == epochs-1:
-#                 debug_z = sess.run(
-#                     [z],
-#                     feed_dict={x:batch_x})
-#                 print debug_z
               
         print "epoch: {0} accuracy: {1}".format(epoch, float(total_hit)/n_samples)
+         
+        if epoch == epochs-1:
+            debug_z, debug_loss_z_entropy, debug_loss_z_mean, debug_loss_classifier_y_x, debug_loss_classifier = sess.run(
+                [z, loss_z_entropy, loss_z_mean, loss_classifier_y_x, loss_classifier],
+                feed_dict={x:batch_x, mask:batch_mask, y_label:batch_y_label, y_prior:batch_majority_y, y_kl_strength:0.0001})
+#            print debug_z
+            print "loss_z_entropy:     ", debug_loss_z_entropy
+            print "loss_z_mean:        ", debug_loss_z_mean
+            print "loss_classifier_y_x:", debug_loss_classifier_y_x
+            print "loss_total:         ", debug_loss_classifier
             
 print "Done!" 
